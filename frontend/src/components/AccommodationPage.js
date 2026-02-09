88c68ad294ae605e8019eca0d6328eaa10002465
samruddhi <samruddhisr4@gmail.com>
2026-02-09 11:45:57 +0530

    update

--- frontend/src/components/AccommodationPage.js
+++ frontend/src/components/AccommodationPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './AccommodationPage.css';
 
 const AccommodationPage = ({ travelPlan, onComponentGenerate, loadingComponent }) => {
